87f021d2cd2314afde91f81e5acad1e07b222198
alain <xalaindimabuyo@gmail.com>
2019-01-06 21:52:33 +0800

    second

---
+++ src/robots.js
@@ -0,0 +1,33 @@
+export const robots = [
+        {
+            id: 1,
+            name: 'Alain Dimabuyo',
+            username: 'Lainpogi',
+            email: 'xalaindimabuyo@gmail.com',
+        },
+        {
+            id: 2,
+            name: 'Andrei Dimabuyo',
+            username: 'dreipogi',
+            email: 'xandrei@gmail.com',
+        },
+        {
+            id: 3,
+            name: 'Antonia Dimabuyo',
+            username: 'Antonia asd',
+            email: 'xantonia@gmail.com',
+        },
+        {
+            id: 4,
+            name: 'Ariel Dimabuyo',
+            username: 'ariel pogi',
+            email: 'xarieldimabuyo@gmail.com',
+        },
+        {
+            id: 5,
+            name: 'Asntro Dimabuyo',
+            username: 'asntro pogi',
+            email: 'xarieldimabuyo@gmail.com',
+        }
+
+    ]
